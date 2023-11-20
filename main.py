d60263314fe991442643125364ed166a284edb83
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2023-11-20 17:00:57 +0900

    알고리즘 노트 수정

--- main.py
+++ main.py
@@ -1,49 +1,15 @@
 import sys
-import heapq
-from collections import deque
-from itertools import combinations
-import copy
-from bisect import bisect_left, bisect_right
 
 sys.setrecursionlimit(10**6)
 sys.stdin=open("input.txt", "r")
 
-
-
-
-def check(x):
-    sumVal = 0
-    for a in arr:
-        if a <= x:
-            sumVal += a 
-        else:
-            sumVal += x 
-    else:
-        return sumVal
-
-if __name__ == "__main__":
-    n = int(input())
-   # dy[길이][끝나는수]
-    dy  = [[0] * (n+1) for i in range(10)]
-
-    for i in range(1, 10):
-        dy[i][1] = 1
-    dy[0][1] = 0
-
-    for j in range(2, n+1):
-        for i in range(10):
-            if i == 0:
-              dy[i][j] = dy[i+1][j-1]
-            elif i == 9:
-              dy[i][j] = dy[i-1][j-1]
-            else:
-              dy[i][j] += dy[i+1][j-1]
-              dy[i][j] += dy[i-1][j-1]
-    res = 0
-    for i in range(10):
-      res += dy[i][n]
-    print(res % 1000000000)
-
+N, K = map(int, input().split())
+res = 0
+while(N >= K):
+    res += (N-(N//K)*K)
+    N -= (N-(N//K)*K)
+    N = N // K 
+    res += 1
+res += (N-1)
+print(res)
     
-
-        
--- test.py
+++ test.py
@@ -1,6 +1,56 @@
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-s = 3
-e = 9
-print((e+s+1)//2)
-for i, v in enumerate([5, 6, 7]):
-    print(i, v)
+import sys
+
+sys.setrecursionlimit(10**6)
+from collections import deque
+from  itertools import combinations
+
+
+board = [list(map(int, input().split())) for _ in range(10)]
+
+
+paper = [0] * 6
+def D(x, y):
+  global ans 
+
+  if x == 10:
+    ans = min(ans, sum(paper))
+    return
+  if y == 10:
+    D(x+1, 0)
+    return
+
+  if board[x][y] == 0:
+    D(x, y+1)
+    return
+
+  for sz in range(1, 6):
+    if is_possible(x, y, sz):
+      mark(x, y, sz, 0)
+      D(x, y+1)
+      mark(x, y, sz, 1)
+
+  
+def is_possible(x, y, sz):
+
+  if paper[sz] == 5:
+    return False 
+  if x + sz > 10 or y + sz > 10:
+    return False 
+  
+  for i in range(sz):
+    for j in range(sz):
+      if board[x+i][y+j] != 1:
+        return False 
+  return True
+def mark(x, y, sz, v):
+  for i in range(sz):
+    for j in range(sz):
+      board[x+i][y+j] = v
+  if v:
+    paper[sz] -= 1
+  else:
+    paper[sz] += 1
+  
+ans = 25 
+D(0, 0)
+print(-1 if ans == 25 else ans)
