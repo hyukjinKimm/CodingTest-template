31abeff70a0b96b56ca8c8df85c2cca2219b128d
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2023-11-22 17:40:38 +0900

    알고리즘 노트 수정

--- main.py
+++ main.py
@@ -4,30 +4,42 @@ from itertools import combinations
 sys.setrecursionlimit(10**6)
 sys.stdin=open("input.txt", "r")
 
-def solution(food_times, k):
-    if sum(food_times) <= k:
-        return -1
-    length = len(food_times)
-    foodAndIndex = []
-    for i in range(length):
-        foodAndIndex.append((food_times[i], i))
-    foodAndIndex.sort()
-    q = deque(foodAndIndex)
-    previous = 0
-    while((q[0][0] - previous) * (length) <= k):
-        time, index = q.popleft()
-        k -= (time-previous) * (length)
-        length -= 1
-        previous = time 
-  
-    tmp = sorted(q, key=lambda x:x[1])
-    
-    return tmp[k % length][1] + 1
-   
-  
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
 
 
 if __name__ == "__main__":
-    food_array = 			[2, 6, 5, 1, 2]
-    k = 10
-    print(solution(food_array, k))
+  n, _ = map(int, input().split())
+  board = []
+  for i in range(n):
+    board.append(list(map(int, sys.stdin.readline().strip().split())))
+  visited = [[0] * n for _ in range(n)]
+  s, target_x, target_y = map(int, input().split())
+  q = []
+  for i in range(n):
+    for j in range(n):
+      if board[i][j] != 0:
+        visited[i][j] = 1
+        q.append((i, j, board[i][j], 0))
+  q.sort(key=lambda x:x[2])
+  
+  q = deque(q)
+  while(q):
+    x, y, k, now = q.popleft()
+    if now == s:
+
+      print(board[target_x-1][target_y-1])
+      break
+
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == 0 and board[nx][ny] == 0:
+        visited[nx][ny] = 1
+        board[nx][ny] = k 
+        q.append((nx, ny, k, now+1))
+  else:
+      print(board[target_x-1][target_y-1])
+    
+        
--- test.py
+++ test.py
@@ -4,30 +4,16 @@ from collections import deque
 from itertools import combinations
 sys.setrecursionlimit(10**6)
 sys.stdin=open("input.txt", "r")
-def solution(food_times, k):
-    if sum(food_times) <= k: # 전체 음식을 먹는 시간보다 k가 크거나 같으면 -1
-        return -1
-    
-    q = [] 
-    for i in range(len(food_times)):
-        heapq.heappush(q, (food_times[i], i+1)) # (음식시간, 음식번호)를 우선순위 큐에 삽입
-        
-    sum_value = 0 #  먹기 위해 사용한 시간
-    previous = 0 # 직전에 다 먹은 음식 시간
-    length = len(food_times) # 남은 음식의 개수
-    
-    # "먹기 위해 사용한 시간 + (현재 음식 시간 - 이전 음식 시간) * 현재 남은 음식 개수" 와 "k" 비교
-    while sum_value + ((q[0][0] - previous) * length) <= k: 
-        now = heapq.heappop(q)[0] # 현재 음식 시간
-        sum_value += (now - previous) * length 
-        length -= 1 # 다 먹은 음식 제외
-        previous = now # 이전 음식을 현재 음식으로
-        
-    result = sorted(q, key = lambda x: x[1]) # 음식 번호 기준으로 정렬하여 저장
-    
-    return result[(k - sum_value) % length][1] # 반복문을 돌지는 않지만,
+def rotate_90(m):
+    N = len(m)
+    ret = [[0] * N for _ in range(N)]
+    # 왜 'ret = [[0] * N] * N'과 같이 하지 않는지 헷갈리시면 연락주세요.
 
+    for r in range(N):
+        for c in range(N):
+            ret[c][N-1-r] = m[r][c]
+    return ret
 if __name__ == "__main__":
-    food_array = [2, 6, 5, 1, 2]
-    k = 10
-    print(solution(food_array, k))
+  test = [[1,2,3], [4,5,6], [7,8,9]]
+  
+  print(rotate_90(test))
