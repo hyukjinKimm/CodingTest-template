a92fb179452d91012e766e5b1a2155fc396ff68e
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2023-12-04 18:33:06 +0900

    알고리즘 노트 수정

--- main.py
+++ main.py
@@ -21,37 +21,52 @@ sys.stdin=open("input.txt", "r")
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-def dfs(x, y, dist):
-  global res
-  res = max(dist, res)
-  for i in range(4):
-    nx = x + dx[i]
-    ny = y + dy[i]
-    if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0 and history[ord(board[nx][ny])-65] == 0:
-      history[ord(board[nx][ny])-65] =1
-      visited[nx][ny] = 1
-      dfs(nx, ny, dist+1)
-      visited[nx][ny] = 0 
-      history[ord(board[nx][ny])-65] = 0
 
+
+
+    
 
 if __name__ == "__main__":
   n, m = map(int, sys.stdin.readline().strip().split())
   board = []
   for _ in range(n):
-    board.append(list(sys.stdin.readline().strip()))
-  visited = [[0] * m for _ in range(n)]
-  visited[0][0] = 1
-  history = [0] * (26)
-  history[ord(board[0][0])-65] = 1
-  res = -1
-  dfs(0, 0, 1)
-  print(res)
-  
+    board.append(list(map(int,list(sys.stdin.readline().strip()))))
+
   
 
+  visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
+  
+  
+  visited[0][0][0] = 1
 
 
+  c = 0 
+  q = deque([])
+  q.append((0, 0, 1, 0))
+
+  
+  while(q):
+    x, y, d, c= q.popleft() # c = 1 벽을 뿌수고옴 c = 0 벽을 안뿌수고 옴.
+
+    if x == n-1 and y == m-1:
+      print(d)
+      break
+    for i in range(4):
+      nx = x  + dx[i]
+      ny = y + dy[i]
+      if 0 <= nx < n and 0 <= ny < m:
+     
+        if board[nx][ny] == 1 and c == 0 and visited[nx][ny][1] == 0:
+            visited[nx][ny][1] = 1
+            q.append((nx, ny, d+1, 1))
+        elif board[nx][ny] == 0 and visited[nx][ny][c] == 0:
+     
+          visited[nx][ny][c] = 1
+          q.append((nx, ny, d+1, c))
+          
+  else:
+    print(-1)
+ 
 
 
 
@@ -59,7 +74,3 @@ if __name__ == "__main__":
 
 
       
-
-
-
-    
