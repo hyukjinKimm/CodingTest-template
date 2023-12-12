a4c44c1d146c826cade6803583b78af390088fd9
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2023-12-12 20:10:04 +0900

    알고리즘 노트 수정

--- main.py
+++ main.py
@@ -38,12 +38,21 @@ input = sys.stdin.readline
     
 
         
+def dfs(x, s, t):
+  global res
+  if t > k:
+    return
+  if x == n:
+    res = max(res, s)
+    return
+
+  dfs(x+1, s+st[x][0], t+st[x][1])
+  dfs(x+1, s, t)
 if __name__ == "__main__":
   n, k = map(int, input().strip().split())
-  
-  for i in range(1, n+1):
-    if n % i == 0:
-      k -= 1
-      if k == 0:
-        print(i)
-        break
+  st = []
+  for i in range(n):
+    st.append(list(map(int, input().strip().split())))
+  res = -1
+  dfs(0,0,0)
+  print(res)
--- 그 외 함수.py
+++ 그 외 함수.py
@@ -62,3 +62,11 @@ def getMyDivisor(n):
                 res += 1
 
     return res
+
+# 이항계수 구하기 
+def bino_coef(n, k):
+    if k == 0 or n == k:
+        return 1
+    return bino_coef(n-1, k) + bino_coef(n-1, k-1)
+# bino_coef(4, 2)
+# 6
