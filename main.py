2c2b2373f8695cc06a0632c72ed7bd44ec144a70
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2023-12-25 18:25:46 +0900

    알고리즘 노트 수정

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from itertools import product
 from itertools import combinations_with_replacement
 from bisect import bisect_left, bisect_right
 import re
-from bisect import bisect_left, bisect_right
+
 
 import math
 
@@ -25,43 +25,38 @@ dy = [0, 1, 0, -1]
 
 
 
-
-
+def find_parent(parent, x):
+  # 루트 노드가 아니면, 루트 노드를 찾을때까지 재귀적으로 찾기
+  if parent[x] != x:
+    parent[x] = find_parent(parent, parent[x])
+  return parent[x]
+ 
+# 두 원소가 속한 집합을 합치기
+def union_Parent(parent, a, b):
+  a = find_parent(parent, a)
+  b = find_parent(parent, b)
+  if a < b:
+    parent[b] = a
+  else:
+    parent[a] = b
 input = sys.stdin.readline
-   
-
+  
 
 
 if __name__ == "__main__":
-  for _ in range(int(input().strip())):
-    f = input().strip()
-    x = int(input().strip())
-    if x:
-      arr = input().strip()[1:-1].split(',')
-    else:
-      input()
-      arr = []
+  v, e = map(int, input().split())
+  parent = [0] * (v + 1)
   
-    arr = deque(arr)
-    flag = True
-    for o in f:
-      if o == 'R':
-        flag = not flag 
-      else:
-        if not arr:
-          print('error')
-          break
-        if flag:
-          arr.popleft()
-        else:
-          arr.pop()
-    else:
-      arr = list(arr)
-      if flag:
-        print('[' + ",".join(arr)+ "]")
-      else:
-       print('[' + ",".join(arr[::-1])+ "]")
-
-   
-    
-    
+  for i in range(1, v + 1):
+    parent[i] = i
+  
+  for i in range(e):
+    a, b = map(int, input().split())
+    union_Parent(parent, a, b)
+  
+  #이걸 해줘야 부모 테이블이 루트 노드로 전부 업데이트 된다.
+  
+  for i in range(1, v + 1):
+    find_parent(parent, i)
+  
+  print(len(set(parent[1:])))
