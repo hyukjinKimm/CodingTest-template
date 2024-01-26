cbce90a654d4efaf6897e315104d766e4e8e8787
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-01-26 17:44:09 +0900

    알고리즘 노트 수정

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from itertools import permutations
 from itertools import product
 import heapq
 import math
-dx = [-1, 1, 0, 0]
-dy = [0, 0, 1, -1]
+
 
 sys.setrecursionlimit(10**6)
 sys.stdin=open("input.txt", "r")
@@ -25,34 +24,40 @@ input = sys.stdin.readline
 
 
 
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
 
 
-if __name__=="__main__":
-  s = input().strip()
-
-  d = dict()
-  for c in s:
-    if d.get(c):
-      d[c] += 1
-    else:
-      d[c] = 1
-  cnt = 0
-  hol = ''
-  for items in d.items():
-    if items[1] % 2 != 0:
-      cnt += 1
-      hol = items[0]
-    if cnt == 2:
-      print("I'm Sorry Hansoo")
-      sys.exit()
-  arry = list(d.items())
-  arry.sort()
-  res = ''
-  for c, v in arry:
-    res += c*(v//2)
-  print(res + hol + res[::-1])
     
-  
 
 
+      
+           
+def solution(n, weak, dist):
+    lw = len(weak)
+    res = float('inf')
+    for i in range(lw):
+       weak.append(weak[i]+n)
+    for friends in permutations(dist, len(dist)):
+       for start in range(lw):
+          now = 0
+          until = 0
+          for pos in range(start, start+lw):
+            if weak[pos] > until:
+              now += 1
+              if now > len(dist):
+                  break
+              until = weak[pos] + friends[now-1]
+          else:
+             res = min(now, res)
+
+          
+    if res == float('inf'):
+       return -1
+    return res
+if __name__=="__main__":
+  n = 12
+  weak = 	[1, 3, 4, 9, 10]
+  dist = [3, 5, 7]
+  print(solution(n, weak, dist))
