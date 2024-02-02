e886d511bcae2545e48c37df936e457d3cbe884b
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-02-02 21:58:51 +0900

    알고리즘 노트 수정

--- main.py
+++ main.py
@@ -37,58 +37,48 @@ dy = [0, 0, -1, 1]
 
 
     
-
-
+# 다익스트라 알고리즘 - 방문처리여부를 확인 필요X
+def dijkstra(start):
+    q = []
     
+	#시작 노드로 가기위한 최단경로는 0으로 설정하고 (우선순위)큐에 삽입
+    heapq.heappush(q,(0, start)) 
+    distance[start] = 0
 
+	# 큐가 빌때까지
+    while q:
+        dist, now = heapq.heappop(q) # 거리가 가장 짧은 노드를 큐에서 꺼낸다.
+        # 현재 노드가 이미 처리된적 있는 노드라면 무시 -> 방문이 되었는지 확인하는것과 같은원리
+        # 현재 꺼낸 그 원소의 거리값(dist)이 테이블에 기록되어있는 값보다 더 크다면 이미 처리된것
+        if distance[now] < dist:
+          continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]: #현재노드를 거쳐가는것과 기존의 값을 비교
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
-  
-
-
-
-        
-
-
-
-  
-
-
-
-
-  
-def dfs(x, y, c, s):
-  global res
-  if c == 4:
-    res = max(res, s)
-    return 
-  
-  for i in range(4):
-    nx = x + dx[i]
-    ny = y + dy[i]
-    if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0:
-
-      visited[nx][ny] = 1
-      if c == 2:
-        dfs(x, y, c+1, s+board[nx][ny])
-        
-      
-      dfs(nx, ny, c+1, s+board[nx][ny])
-      visited[nx][ny] = 0
-      
-
-
-  
-
+INF = float('inf')
 if __name__=="__main__":
   n, m = map(int, input().strip().split())
-  board = []
-  for _ in range(n):
-    board.append(list(map(int, input().strip().split())))
-  visited = [[0] * m for _ in range(n)]
-  res = -1
-  for i in range(n):
-    for j in range(m):
-      visited[i][j] = 1
-      dfs(i,j,1,board[i][j])
-      visited[i][j] = 0
-  print(res)
+  start = int(input().strip())
+  graph = [[] for _ in range(n+1)]
+  for _ in range(m):
+    a, b, c = map(int, input().strip().split())
+    graph[a].append((b, c))
+  distance = [INF] * (n+1)
+  dijkstra(start)
+  for x in distance[1:]:
+     if x == INF:
+        print(-1)
+     else:
+        print(x)
+
+
+
+
+
+
+
+  
+    
