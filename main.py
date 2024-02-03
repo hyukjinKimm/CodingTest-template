b1aa2921b13accb4eae717a950c24540e2c347f0
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-02-03 16:40:35 +0900

    알고리즘 노트 수정

--- main.py
+++ main.py
@@ -25,60 +25,27 @@ dy = [0, 0, -1, 1]
 
 
 
-      
+
+INF = int(1e9)
 
 
+def ans(row, col):
+  if dy[row][col] != -1: return dy[row][col]
+  if row == 0: return board[row][col]
+  for i in [-1, 0, 1]:
+    nr = row - 1
+    nc = col + i 
+    if 0 <= nc < 3:
+      dy[row][col] = max(dy[row][col], ans(nr, nc) + board[row][col])
+  return dy[row][col]
 
-
-
-
-
-
-
-
-    
-# 다익스트라 알고리즘 - 방문처리여부를 확인 필요X
-def dijkstra(start):
-    q = []
-    
-	#시작 노드로 가기위한 최단경로는 0으로 설정하고 (우선순위)큐에 삽입
-    heapq.heappush(q,(0, start)) 
-    distance[start] = 0
-
-	# 큐가 빌때까지
-    while q:
-        dist, now = heapq.heappop(q) # 거리가 가장 짧은 노드를 큐에서 꺼낸다.
-        # 현재 노드가 이미 처리된적 있는 노드라면 무시 -> 방문이 되었는지 확인하는것과 같은원리
-        # 현재 꺼낸 그 원소의 거리값(dist)이 테이블에 기록되어있는 값보다 더 크다면 이미 처리된것
-        if distance[now] < dist:
-          continue
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]: #현재노드를 거쳐가는것과 기존의 값을 비교
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-
-INF = float('inf')
 if __name__=="__main__":
-  n, m = map(int, input().strip().split())
-  start = int(input().strip())
-  graph = [[] for _ in range(n+1)]
-  for _ in range(m):
-    a, b, c = map(int, input().strip().split())
-    graph[a].append((b, c))
-  distance = [INF] * (n+1)
-  dijkstra(start)
-  for x in distance[1:]:
-     if x == INF:
-        print(-1)
-     else:
-        print(x)
+  n = int(input().strip())
+  board =[]
+  for _ in range(n):
+    board.append(list(map(int, input().strip().split())))
+  dy = [[-1] * 3 for _ in range(n)]
+  for i in range(3):
+    ans(n-1, i)
+  print(dy[n-1])
 
-
-
-
-
-
-
-  
-    
