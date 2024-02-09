b63fc2d76eaab98e88a77d17c09a94931fa90fad
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-02-09 15:58:16 +0900

    알고리즘 노트 수정

--- main.py
+++ main.py
@@ -25,28 +25,34 @@ dy = [0, 0, -1, 1]
 
 
 
-
-INF = int(1e9)
+INF = float('inf')
 
 mod = 1000000007
 
   
 
-    
+  
+      
+
+def dfs(now, acc):
+  global res
+  if now > 1000000000: return 
+  if now == b:
+    res = min(res, acc)
+    return 
+  
+  dfs(now*2, acc+1)
+  dfs(10*now+1, acc+1)
 
 if __name__=="__main__":
-  n, m = map(int, input().strip().split())
-  arr = []
-  for _ in range(n):
-    arr.append(list(map(int, input().strip().split())))
-  dy = [[0] * (n+1) for _ in range(n+1)]
-  for i in range(1, n+1):
-    dy[1][i] = dy[1][i-1] + arr[0][i-1]
-    dy[i][1] = dy[i-1][1] + arr[i-1][0]
-  for i in range(1, n+1):
-    for j in range(1, n+1):
-      dy[i][j] = dy[i-1][j] + dy[i][j-1] - dy[i-1][j-1] + arr[i-1][j-1]
-  for _ in range(m):
-    x1, y1, x2, y2 = map(int, input().strip().split())
-    print(dy[x2][y2] + dy[x1-1][y1-1] - dy[x1-1][y2] - dy[x2][y1-1])
+  a, b = map(int, input().strip().split())
+  res = INF
+  dfs(a, 0)
+  if res == INF:
+    print(-1)
+  else:
+    print(res+1)
+  
+
+
   
